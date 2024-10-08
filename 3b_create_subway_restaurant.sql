3621d420ef9c778fc21653f5a81ea21b1b45859d
Student <jsantoscustodio@appdev.yearup.org>
2024-10-07 20:30:03 -0500

    updated 3B exercise

--- 3b_create_subway_restaurant.sql
+++ 3b_create_subway_restaurant.sql
@@ -1,10 +1,35 @@
-CREATE DATABASE Subway_Restaurant;
+# 3.B Creating your database #
 
-CREATE TABLE customers (
-CustomerID SMALLINT PRIMARY KEY AUTO_INCREMENT,
-FirstName VARCHAR(255),
-LastName VARCHAR(255),
-CustomerPhone VARCHAR(255), 
-CustomerEmail VARCHAR(255), 
-Last_Update TIMESTAMP
+DROP DATABASE IF EXISTS BreeThai;
+
+CREATE DATABASE IF NOT EXISTS BreeThai; 
+
+USE BreeThai;
+
+# Adding three Tables # 
+
+# Add Table "Customers" #
+# AUTO INCREMENT CUSTOMER_ID as primary key and make smalliant to use less storage #
+# Make not null firstname,lastname, customeremail, and custome phone as they can refer back to purchase details #
+# Assign current timestamp as the default value to specify the current timestamp and auto update values to show when the table was last updated # 
+
+
+
+CREATE TABLE `customers` (
+`Customer_ID` SMALLINT PRIMARY KEY AUTO_INCREMENT,
+`FirstName` VARCHAR(100) NOT NULL,
+`LastName` VARCHAR(100) NOT NULL,
+`CustomerPhone` VARCHAR(100) NOT NULL, 
+`CustomerEmail` VARCHAR(255) NOT NULL,
+`Last_Update` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
  );
+
+# Add Table Sandwich Orders #
+# Primary key will be sandiwich_ID and it will be auto increment#
+# Customer_ID will be the foreign key # 
+# included not null for meat type, bread type, and ingredients, as this is important stock information for inventory table #
+# product_ID will be smalliant to use less storage # 
+# Assign current timestamp as the default value to specify the current timestamp and auto update values to show when the table was last updated # 
+
+
+
